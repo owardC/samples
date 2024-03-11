952680947c2943b7dc6b6a1b86af5211c670d7e7
owardC <97756819+owardC@users.noreply.github.com>
2024-03-11 19:44:15 -0400

    Add files via upload

---
+++ application/dji_sdk_app_info.h
@@ -0,0 +1,55 @@
+/**
+ ********************************************************************
+ * @file    dji_sdk_app_info.h
+ * @brief   This is the header file for defining the structure and (exported) function prototypes.
+ *
+ * @copyright (c) 2018 DJI. All rights reserved.
+ *
+ * All information contained herein is, and remains, the property of DJI.
+ * The intellectual and technical concepts contained herein are proprietary
+ * to DJI and may be covered by U.S. and foreign patents, patents in process,
+ * and protected by trade secret or copyright law.  Dissemination of this
+ * information, including but not limited to data and other proprietary
+ * material(s) incorporated within the information, in any form, is strictly
+ * prohibited without the express written consent of DJI.
+ *
+ * If you receive this source code without DJI’s authorization, you may not
+ * further disseminate the information, and you must immediately remove the
+ * source code and notify DJI of its removal. DJI reserves the right to pursue
+ * legal actions against you for any loss(es) or damage(s) caused by your
+ * failure to do so.
+ *
+ *********************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef DJI_SDK_APP_INFO_H
+#define DJI_SDK_APP_INFO_H
+
+/* Includes ------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Exported constants --------------------------------------------------------*/
+// ATTENTION: User must goto https://developer.dji.com/user/apps/#all to create your own dji sdk application, get dji sdk application
+// information then fill in the application information here.
+#define USER_APP_NAME               "m30 dropkit"
+#define USER_APP_ID                 "143863"
+#define USER_APP_KEY                "8e95994a36f98e963255dec210f4194"
+#define USER_APP_LICENSE            "TBJviiD6X9jyVYZpIt0A7GLcUbJE6AMcC3ndygbr+zpLv7robSIyKxpjwmTgBh1thMveOdUmZaLxmoi3EoL5IkvOebb4pMOpjfwmhCoA5KRFAbOF8E2nioBFfpGrHKMIQOj9eqNHYt41AYiYk1yCGyoz07ccYWwKchGCc62AgGp8WFEBjcPL+2osQlzWUVoEJajrJgqoWDjehoILOAowxFfJ2MprAaGIyAvSsuNLqQ3BA691SsCwvA+38p4b/sDPjD+sBxaohwI2vriR8K+8iWeVItHkmmqooOxRZBHhvBj58hflJVoq4ZPR3ZIK8rRAYF4oD9/AEkNsSGhqCZbUnw=="
+#define USER_DEVELOPER_ACCOUNT      "cadenas3d@gmail.com"
+#define USER_BAUD_RATE              "460800"
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DJI_SDK_APP_INFO_H
+/************************ (C) COPYRIGHT DJI Innovations *******END OF FILE******/
